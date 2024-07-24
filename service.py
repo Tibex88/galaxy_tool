d0b8546c6582c17c19b0c5d66a9cb850810ba123
Tibex88 <tibesolomon7@gmail.com>
2024-07-24 18:52:14 +0300

    added elements

--- service.py
+++ service.py
@@ -5,17 +5,23 @@ def main(argv=None):
     if argv is None:
         argv = sys.argv[1:]
     args = _parser().parse_args(argv)
-    print(args.d,args.str, args.file, args.bool)
+    # print(args.d,args.str, args.file, args.bool)
+    # print("Query: ",type(args.query))
+    # print("Query: ",type(args.query[0]))
+    # print("Query: ",(args.query[0]))
+    print("Query: ",(args.query))
+    print("Prompt: ",args.prompt)
     exit_code = 0
     sys.exit(exit_code)
-    
+
 def _parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--d", type=str, help="directory target URI")
-    parser.add_argument("--str", type=str, help="file sources json")
-    parser.add_argument("--file", type=str, help="files to export")
-    parser.add_argument("--bool", type=bool, help="export metadata files", default=True)
-    # parser.add_argument("--select", type=str, help="select type")
+    # parser.add_argument("-d", type=str, help="directory target URI")
+    # parser.add_argument("-str", type=str, help="file sources json")
+    # parser.add_argument("-file", type=str, help="files to export")
+    parser.add_argument("-prompt", type=str, help="NL prompt", default='No prompt')
+    parser.add_argument("-query",nargs='+' , type=str, help="list of arguments", default='No query')
+    # parser.add_argument("-select", type=str, help="select type")
     return parser
 
 
